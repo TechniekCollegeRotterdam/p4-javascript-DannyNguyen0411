41683e91827ea3c0d12dde17bf988808a7bded68
DannyNguyen0411 <90318109+DannyNguyen0411@users.noreply.github.com>
2022-05-16 14:03:36 +0100

    manier 1 en manier 2

--- app.js
+++ app.js
@@ -1,21 +1,49 @@
-let name;
-let country;
+////manier 1
+// let name;
+// let country;
+// let p = document.getElementById("output");
+
+// const nameChanged = function(event){
+//    name = event.target.value;
+//    changeInput();
+// }
+
+// const countryChange = function(event){
+//     country = event.target.value;
+//     changeInput();
+//  }
+
+//  const changeInput = function(){
+//    const text = `Je heet ${name} en je komt uit ${country}`;
+//    p.textContent = text;
+//  } 
+
+////manier 2
+// let button = document.querySelector("button");
+
+// button.addEventListener("click", funtion(){
+// console.log("geklikt");
+// });
+
+let name = document.querySelector("nameChanged");
+let country = document.querySelector("countryChanged");
 let p = document.getElementById("output");
 
-const nameChanged = function(event){
+nameChanged.addEventListener("change", function(){
    name = event.target.value;
    changeInput();
-}
+})
 
-const countryChange = function(event){
-    country = event.target.value;
-    changeInput();
- }
+countryChanged.addEventListener("change", function(){
+   country = event.target.value;
+   changeInput();
+})
 
- const changeInput = function(){
+const changeInput = function(){
    const text = `Je heet ${name} en je komt uit ${country}`;
    p.textContent = text;
- } 
+}
+
 
 
  
