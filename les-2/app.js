f23ae6afd446653198f6eea000c181db73d437fb
DannyNguyen0411 <90318109+DannyNguyen0411@users.noreply.github.com>
2022-05-20 11:16:19 +0100

    bijna klaar oefening 2

--- les-2/app.js
+++ les-2/app.js
@@ -12,3 +12,27 @@ bird.addEventListener("click", function(){
 
 })
 
+window.addEventListener("keydown", function(event){
+    //aan de bird class word styling toegevoegd
+    //de styling is left 50px
+    if(event.key == "ArrowRight"){
+    count += 50;
+    bird.style.left = count + "px";
+
+    } 
+    if(event.key == "ArrowLeft"){
+    count -= 50;
+    bird.style.left = count + "px";
+
+    }
+    if(event.key == "ArrowUp"){
+    count += 50;
+    bird.style.bottom = count + "px";
+
+    }
+    if(event.key == "ArrowDown"){
+    count += 50;
+    bird.style.top = count + "px";
+    }
+})
+
